8688bc4e6ec6043c93974853cc0d39859566fd6d
abantikaguin <abantikaguin23@gmail.com>
2024-11-29 21:33:45 +0530

    Add files via upload

---
+++ SCT_ML_01.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.impute import SimpleImputer
+
+train = pd.read_csv('E:\\House price\\train.csv')
+test = pd.read_csv('E:\\House price\\test.csv')
+
+X = train[['LotArea', 'BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', '1stFlrSF', '2ndFlrSF',
+           'LowQualFinSF', 'GrLivArea', 'FullBath', 'HalfBath', 'BedroomAbvGr', 'GarageArea', 'WoodDeckSF',
+           'OpenPorchSF', 'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea']]
+y = train['SalePrice']
+Xtest = test[['LotArea', 'BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', '1stFlrSF', '2ndFlrSF',
+              'LowQualFinSF', 'GrLivArea', 'FullBath', 'HalfBath', 'BedroomAbvGr', 'GarageArea', 'WoodDeckSF',
+              'OpenPorchSF', 'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea']]
+
+model = LinearRegression()
+
+imput = SimpleImputer(strategy='mean')
+X = imput.fit_transform(X)
+Xtest = imput.transform(Xtest)
+
+model.fit(X, y)
+pred = model.predict(Xtest)
+output = pd.DataFrame({'Id': test['Id'], 'SalePrice': pred})
+output.to_csv('E:\\House price\\predictions.csv', index=False)
+print(output)
